60aff46b581b7c18db077b42045b3d32c3d6c97a
Miloslav Matulka <miloslav.matulka@gmail.com>
2018-03-08 13:35:30 +0100

    blossom.py v1.0
    Rendering of blossom using for loops - 18 squares.

---
+++ blossom.py
@@ -0,0 +1,11 @@
+from turtle import speed, forward, left, exitonclick
+
+speed(10)
+
+for square in range(18):
+    for j in range(4):
+        forward(50)
+        left(90)
+    left(20)
+        
+exitonclick()
