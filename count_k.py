54dcbb90e8cbb0529d20c7b3c7a803571d479ede
Miloslav Matulka <miloslav.matulka@gmail.com>
2018-03-09 10:52:25 +0100

    count_k.py v1.0
    We want to return a count of the letter "k" in our text regardless
    upper/lower case.

---
+++ count_k.py
@@ -0,0 +1,18 @@
+"""
+This program returns the count of letter "k" in a song.
+
+"""
+
+text = """
+Words like violance
+Break the silence
+Come crashing in
+Into my little world
+Painful to me
+Pierce right through me
+Can't you understand
+Oh, my little girl.
+"""
+
+text_lowercase = text.lower()
+print(text_lowercase.count("k"))
