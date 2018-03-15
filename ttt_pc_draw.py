1cebda0559469965be6b32d05bc1f01dfcef424e
Miloslav Matulka <miloslav.matulka@gmail.com>
2018-03-15 14:26:02 +0100

    ttt_pc_draw.py v1.1
    Added offensive and defensive strategy so that PC is hard to defeat.

--- ttt_pc_draw.py
+++ ttt_pc_draw.py
@@ -18,6 +18,30 @@ def pc_draw(field):
             if field[number] == "x" or field[number] == "o":
                 # No need to execute: print("Position is already populated.")
                 continue
+            elif "-oo" in field:
+                number = field.index("-oo")
+                break
+            elif "o-o" in field:
+                number = field.index("o-o") + 1
+                break
+            elif "oo-" in field:
+                number = field.index("oo-") + 2
+                break
+            elif "-xx" in field:
+                number = field.index("-xx")
+                break
+            elif "x-x" in field:
+                number = field.index("x-x") + 1
+                break
+            elif "xx-" in field:
+                number = field.index("xx-") + 2
+                break
+            elif "-o-" in field:
+                number = field.index("-o-")
+                break
+            elif "-x-" in field:
+                number = field.index("-x-")
+                break
             else:
                 break
 
