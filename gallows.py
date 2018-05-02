99f5e5990efe7c8bd6a83aad4deec80b1a3768f0
Miloslav Matulka <miloslav.matulka@gmail.com>
2018-05-02 15:53:13 +0200

    gallows.py v1.1
    User has to insert only single letters from alphabet.
    The possibility is given to play again after the end of the game.

--- gallows.py
+++ gallows.py
@@ -96,37 +96,55 @@ r"""
 |  / \
 |
 ~~~~~~~""")
-bad_attempts = 0
-
-print("This is the Gallows game.")
-print("You have to guess a word.")
-set_word = random_word()
-guessed_word = set_word[0]
-hint = set_word[1]
-length = len(guessed_word)
-string = length * "_"
-print(f"The word consists of {length} letters: {string}")
 
 while True:
-    print(f"Hint: {hint}")
-    letter = input("Insert a letter and press enter: ")
-    string = update_string(string, letter, guessed_word)
-    bad_attempts = counter(letter, guessed_word, bad_attempts)
-    print(f"The word is: {string}")
-    print(f"Count of bad attempts: {bad_attempts} of 10")
+    bad_attempts = 0
     
-    # Depicting of the gallows.
-    print(gallows[bad_attempts])
-    
-    evaluation = evaluate(string, bad_attempts)
-    if evaluation == '*':
-        print("Congratulations, you are a winner!\nEnd of game.")
-        break
-    elif evaluation == '!':
-        print("You lost the game!\nEnd of game.")
-        break
-    elif evaluation == '?':
-        print("Let's continue.\n")
+    print("This is the Gallows game.")
+    print("You have to guess a word.")
+    set_word = random_word()
+    guessed_word = set_word[0]
+    hint = set_word[1]
+    length = len(guessed_word)
+    string = length * "_"
+
+    while True:
+        print(f"The word consists of {length} letters. Hint: {hint}")
+        letter = input("Insert a letter and press enter: ").lower()
+        if letter in ('a', 'b', 'c', 'd', 'e', 'f', 'g',
+                      'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                      'o', 'p', 'q', 'r', 's', 't', 'u',
+                      'v', 'w', 'x', 'y', 'z'):
+            pass
+        else:
+            print("Insert a single letter from alphabet! Please try again.")
+            continue
+        string = update_string(string, letter, guessed_word)
+        bad_attempts = counter(letter, guessed_word, bad_attempts)
+        print(f"The word is: {string}")
+        print(f"Count of bad attempts: {bad_attempts} of 10")
+        
+        # Depicting of the gallows.
+        print(gallows[bad_attempts])
+        
+        evaluation = evaluate(string, bad_attempts)
+        if evaluation == '*':
+            print("Congratulations, you are a winner!\nEnd of game.")
+            break
+        elif evaluation == '!':
+            print("You lost the game!\nEnd of game.")
+            break
+        # For evaluation == '?'
+        else:
+            print("Let's continue.\n")
+            continue
+
+    # A new game option.
+    repeat = input("Do you want to play again (y/n)?").lower()
+    if repeat == 'y':
         continue
+    elif repeat == 'n':
+        break
     else:
-        pass
+        print("You pressed neither 'y' nor 'n'! End of game.")
+        break
